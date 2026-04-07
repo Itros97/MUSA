acbbd7887097d11d051a7c9849e99e3eed9b3e44
itros97 <itros97@gmail.com>
2026-04-07 16:47:24 +0200

    Add kotlinx.datetime dependency for date and time handling

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -59,6 +59,7 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+            implementation(libs.kotlinx.datetime)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
